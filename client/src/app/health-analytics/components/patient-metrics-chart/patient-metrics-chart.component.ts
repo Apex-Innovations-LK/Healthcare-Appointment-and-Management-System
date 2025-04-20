9e9e8fe61ca350b18fecf866c2b6ff6b5cb7fa98
seniru-dilmith <dilmithseniru@gmail.com>
2025-04-20 20:49:12 +0530

    basic folder structure for frontend of group 08

---
+++ client/src/app/health-analytics/components/patient-metrics-chart/patient-metrics-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-metrics-chart',
+  imports: [],
+  templateUrl: './patient-metrics-chart.component.html',
+  styleUrl: './patient-metrics-chart.component.css'
+})
+export class PatientMetricsChartComponent {
+
+}
---
+++ client/src/app/health-analytics/health-analytics.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HealthAnalyticsRoutingModule } from './health-analytics-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HealthAnalyticsRoutingModule
+  ]
+})
+export class HealthAnalyticsModule { }
