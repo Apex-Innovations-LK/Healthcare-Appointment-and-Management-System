38aac3d5bf98d50b9b917b38fea03ad59db5e3eb
PamojX <157775896+PamojX@users.noreply.github.com>
2025-05-06 10:15:59 +0530

    Initial commit of Notification Microservice

---
+++ emailproducer/src/test/java/com/kafka/emailproducer/EmailproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.emailproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
