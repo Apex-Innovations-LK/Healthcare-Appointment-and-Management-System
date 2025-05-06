4c3492251f5d7062d4b34f43d85e097dda3383c8
PamojX <157775896+PamojX@users.noreply.github.com>
2025-05-06 10:25:03 +0530

    Added Notification Microservice with Spring Boot and FastAPI Kafka integration

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+<<<<<<< HEAD
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+=======
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+>>>>>>> ccb02e3 (Added Notification Microservice with Spring Boot and FastAPI Kafka integration)
