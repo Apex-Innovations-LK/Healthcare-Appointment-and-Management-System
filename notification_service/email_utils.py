cfd753cc909cf7fbeb48277cd527cd929dc469d7
PamojX <157775896+PamojX@users.noreply.github.com>
2025-05-09 00:27:26 +0530

    Final Error Fixes and working solution on Docker

--- notification_service/email_utils.py
+++ notification_service/email_utils.py
@@ -8,7 +8,8 @@ load_dotenv()
 
 # Get config values from .env
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+# EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
