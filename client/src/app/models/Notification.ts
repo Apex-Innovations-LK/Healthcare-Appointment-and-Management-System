1c90f69cbefa8c65cbc11b9829e6222885b59a31
HarshaYohan <hdankotuwa@gmail.com>
2025-05-10 07:24:07 +0530

    Refactor footer widgets for consistent styling and add notification feature for appointment confirmations

---
+++ client/src/app/models/Notification.ts
@@ -0,0 +1,12 @@
+export class Notification { 
+    to: string;
+    subject: string;
+    body: string;
+
+    constructor(to: string, subject: string, body: string) {
+        this.to = to;
+        this.subject = subject;
+        this.body = body;
+    }
+    
+}
