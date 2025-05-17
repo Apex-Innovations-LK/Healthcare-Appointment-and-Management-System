e835c178dd20f9fda08e0cc34282de33260e049f
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-05-17 22:48:10 +0530

    Refactor Docker configurations and update application properties for resource management service

--- server/health-analytics/src/test/java/com/team8/healthanalyticsforadmin/HealthAnalyticsForAdminApplicationTests.java
+++ server/health-analytics/src/test/java/com/team8/healthanalyticsforadmin/HealthAnalyticsForAdminApplicationTests.java
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:server/health-analytics/src/test/java/com/team8/healthanalyticsforadmin/HealthAnalyticsForAdminApplicationTests.java
 package com.team8.healthanalyticsforadmin;
-========
-package com.team06.appointment_service;
->>>>>>>> 613e115952cf8ac7eb59a53e76bb543f6a537466:server/appointment_service/src/test/java/com/team06/appointment_service/AppointmentServiceApplicationTests.java
-=======
-package com.team8.healthanalyticsforadmin;
->>>>>>> 66d15e7f5a5604550e302747a9e0a3465a2a90db
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<< HEAD
-<<<<<<<< HEAD:server/health-analytics/src/test/java/com/team8/healthanalyticsforadmin/HealthAnalyticsForAdminApplicationTests.java
 class HealthAnalyticsForAdminApplicationTests {
-========
-class AppointmentServiceApplicationTests {
->>>>>>>> 613e115952cf8ac7eb59a53e76bb543f6a537466:server/appointment_service/src/test/java/com/team06/appointment_service/AppointmentServiceApplicationTests.java
-=======
-class HealthAnalyticsForAdminApplicationTests {
->>>>>>> 66d15e7f5a5604550e302747a9e0a3465a2a90db
 
     @Test
     void contextLoads() {
