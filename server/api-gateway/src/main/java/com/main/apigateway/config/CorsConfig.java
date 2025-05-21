e786298197ffaa7b3a53c4c603d5bb41aeb92d4d
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-05-21 07:31:43 +0530

    fix: Update CORS configuration to allow all origins

--- server/api-gateway/src/main/java/com/main/apigateway/config/CorsConfig.java
+++ server/api-gateway/src/main/java/com/main/apigateway/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://35.184.60.72", "0.0.0.0/0")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
